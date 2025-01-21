9716a0036f004a41ce74a7cd4a15c17aee15e784
bstriker <strikeforcezero@gmail.com>
2025-01-21 11:57:41 -0500

    chore(tests): add tests for auto_name with generics

---
+++ tests/nightly/auto_name_with_generics.rs
@@ -0,0 +1,28 @@
+use bevy_app::prelude::*;
+use bevy_auto_plugin_macros::auto_plugin::*;
+use bevy_core::Name;
+use bevy_ecs::prelude::*;
+
+#[derive(Component)]
+#[auto_name(Test<bool>)]
+pub struct Test<T>(T);
+
+#[auto_plugin(app=app)]
+fn plugin(app: &mut App) {}
+
+fn app() -> App {
+    let mut app = internal_test_util::create_minimal_app();
+    app.add_plugins(plugin);
+    app
+}
+
+#[test]
+fn test_auto_name() {
+    let mut app = app();
+    let entity = app.world_mut().spawn(Test(true)).id();
+    app.update();
+    assert_eq!(
+        app.world().entity(entity).get::<Name>(),
+        Some(&Name::new("Test<bool>"))
+    );
+}
